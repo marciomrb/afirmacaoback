b6329fd804d40fcc723c97a981c51b3f8ff0ce1d
Marcio Jr <marciomrbcampos@gmail.com>
2022-03-17 13:10:28 -0300

    package

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const getRandomAffirmation = require('./random_affirmation');
+
+const app = express();
+
+let PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.json({affirmation: getRandomAffirmation()});
+});
+
+const server = app.listen(PORT, () => console.log(`Server is live at localhost:${PORT}`));
+
+module.exports = server;
